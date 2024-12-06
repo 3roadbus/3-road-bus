5d3f3dbbb7ee77f3509cd337a70c505e26bf1396
3roadbus <onettclone12.huang@gmail.com>
2024-12-06 21:09:15 +0800

    Add files via upload

--- Lucas/12/week13.py
+++ Lucas/12/week13.py
@@ -8,6 +8,7 @@ class Player(Sprite):
         self.image = 'USSR.png'
         self.position = Point(650,300)
         self.scale = 0.2
+        self.add_tag('player')
 
     def on_update(self, dt):
         if self.window.is_key_pressed(KeyCode.UP ):
@@ -19,29 +20,44 @@ class Player(Sprite):
         if self.window.is_key_pressed(KeyCode.RIGHT ):
             self.x += 5
 
+player = windows.create_sprite(Player)
+
+        
 class Enemy(Sprite):
     def on_create(self):
         self.image = 'nazi.png'
         self.goto_random_position()
         self.rotation = random.randint(0, 359)
         self.scale = 0.2
-        Scheduler.update(self.shoot, 1 )
-    def shoot(self):
-        windows.create_sprite(enemybullet)
+        self.time = 0
+        self.bullettime = 1
+
     def on_update(self, dt):
-        self.move_forward(3)
-        if self.x > 1200 or self.x < 10 or self.y < 10 or self.y > 1250:
-            self.delete()
+        self.time += dt
+        if self.time > self.bullettime:
+           Enemybullet = windows.create_sprite(enemybullet)
+           self.position = Enemybullet.position
+           Enemybullet.point_toward_sprite(player)
+           self.time = 0
+        if  self.is_touching_window_edge():
+            self.delete
+
+enemy = windows.create_sprite(Enemy)
 
 class enemybullet(Sprite):
     def on_create(self):
         self.image = 'naziflag.png'
         self.position = enemy.position
         self.scale = 0.08
+        
 
     def on_update(self, dt):
-        self.point_toward_sprite(player)
-        
-player = windows.create_sprite(Player)
-enemy = windows.create_sprite(Enemy)
+        self.move_forward(5)
+        if self.is_touching_sprite(player):
+            self.delete()
+
+
+
+
+
 windows.run()
