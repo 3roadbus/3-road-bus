50cfabdf1a6237873b86a8de4df3006d54803e06
3roadbus <onettclone12.huang@gmail.com>
2026-06-18 19:27:40 +0800

    Add files via upload

--- final project- Kaohsiung LRT sim/fp_Kaohsiung_LRT_sim.py
+++ final project- Kaohsiung LRT sim/fp_Kaohsiung_LRT_sim.py
@@ -1,74 +1,39 @@
-from pycat.core import Window, Sprite, Label, Color,Scheduler,KeyCode,Point
+from pycat.core import Window, Sprite, Label, Color,Scheduler
 import random
 
 windows = Window(height=1280,width=2560)
-
-speed = 0
-
 class tram_for_drive(Sprite):
-
     def on_create(self):
         self.image='lrt.png'
         #self.x=-1500
-        self.y=550
+        self.y=640
         self.layer =1
     
-    def on_update(self, dt):
-         #change later
-        global speed
-        if windows.is_key_pressed(KeyCode.NUM_0):
-            if speed<=70:
-                speed += 0.25
-            
-        elif windows.is_key_pressed(KeyCode.NUM_1):
-            if speed>5:
-                speed = speed * 0.9955
-            else:
-                speed = speed *0.97
-        else:
-            speed= speed * 0.99898
-        
-        if speed <0.249:
-             speed = 0
-
 class floor(Sprite):
     def on_create(self):
         self.image='floor.png'
         self.scale=2
         self.x= 1280
         self.color=Color(random.randint(1,255),random.randint(1,255),random.randint(1,255))
-        self.close= False
-
-    def the_building_respawn(self):
-        for i in range(random.randint(1,5)):
-            windows.create_sprite(floor,y=350+i*173,x=2560)
-        self.delete()
 
     def on_update(self, dt):
-        global speed
-        if self.x<25 and self.close== False:
-            self.close=True
-            self.the_building_respawn()
+        if self.x<30:
+            self.delete()
         else:
-            self.x-=speed
+            self.x-=20
 
-def opening_of_buildings():
-    for i in range(8):
-        X=i*363
-        for i in range(random.randint(1,5)):
-            windows.create_sprite(floor,y=350+i*173,x=X)
- 
-#change name later
-class test_text(Label):
+
+
+class building_spawn_point(Sprite):
     def on_create(self):
-        self.position= (1000,1000)
-        self.color= Color.BLUE
-        self.text = str(0)
-        self.layer=2
-    def on_update(self, dt):
-        global speed
-        self.text= str(round(speed))
+        self.x = 2560
+        self.y =400
+        self.layer=0
+        Scheduler.update(lambda : self.genarate_building(random.randint(1,5)),0.3,)  
 
-windows.create_label(test_text)
+    def genarate_building(self,floor_count):
+                for i in range(floor_count):
+                    windows.create_sprite(floor,y=550+i*173, x=2560)
 windows.create_sprite(tram_for_drive)
+windows.create_sprite(building_spawn_point)
 windows.run()
